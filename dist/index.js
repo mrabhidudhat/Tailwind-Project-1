c9057eab9fad52302355e9f732195ccec9cac6dc
mrabhidudhat <mrabhidudhat@gmail.com>
2023-09-26 09:40:03 +0530

    project-1

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+//  <script>
+//     // JavaScript to toggle the menu
+//     const menuToggle = document.getElementById("menu-toggle");
+//     const menuClose = document.getElementById("menu-close");
+//     const menu = document.getElementById("menu");
+
+//     menuToggle.addEventListener("click", () => {
+//       menu.classList.toggle("hidden");
+//       menuToggle.classList.toggle("hidden");
+//       menuClose.classList.toggle("hidden");
+//     });
+
+//     menuClose.addEventListener("click", () => {
+//       menu.classList.toggle("hidden");
+//       menuToggle.classList.toggle("hidden");
+//       menuClose.classList.toggle("hidden");
+//     });
+//   </script>
